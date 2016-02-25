7c5ae290f42c307d2a01139b1180b6ee54f0a79e
Tyler Martinez <slightlytyler@gmail.com>
2016-02-24 22:02:22 -0600

    Better provide decorator

--- src/containers/provide.js
+++ src/containers/provide.js
@@ -10,6 +10,8 @@ import {
 } from 'lodash';
 
 // Create the baes provider object
+// 'constants', 'actions', 'reducers', and 'selectors'
+// will be attached after instantiation
 export function createProvider(type, recordName = type.slice(0, -1), keyName = `${recordName}Key`) {
   return {
     type,
@@ -18,23 +20,22 @@ export function createProvider(type, recordName = type.slice(0, -1), keyName = `
   };
 }
 
-// Returns the string mapped to recordsById
-function byIds(type) {
-  return `${type}ById`;
-}
-
-// Returns a function that accepts a Component instance as the argument
+// Decorator function
+// Accepts the provider object and optional merge function
+// Returns a wrapper function that accepts a Component instance as the argument
 export function provide(provider, merge) {
   const { type, recordName, keyName } = provider;
+  const typeById = `${type}ById`;
+
   const allowedStateProps = [
     type,
-    byIds(type),
+    typeById,
     recordName,
     ...keys(omit(provider.reducers, ['records', 'recordsById'])),
   ];
   const allowedDispatchProps = keys(provider.actions);
 
-  // Return a connected component instance
+  // Return a connected, wrapped HOC
   return function wrap(Component) {
     const requestedProps = keys(Component.propTypes);
     const requestedStateProps = intersection(requestedProps, allowedStateProps);
@@ -43,26 +44,42 @@ export function provide(provider, merge) {
     const mapStateToProps = state => {
       const providerState = state[type];
 
+      // Create inital object mapping requested state props to provider state
       const mappedStateProps = zipObject(requestedStateProps, map(requestedStateProps, prop => {
         switch (true) {
+          // Case where we map 'type' i.e. 'todos'
+          // to 'records', a list of record ids
           case prop === type:
             return providerState.records;
 
-          case prop === byIds(type):
+          // Case where we mape 'typeById' i.e. 'todosById'
+          // to 'recordsById', a key/value map for ids/records
+          case prop === typeById:
             return providerState.recordsById;
 
+          // Default case maps requested state props to
+          // root level keys of provider state
           default:
             return providerState[prop];
         }
       }));
 
+      // If 'recordName' i.e. 'todo' is a requested state prop
       if (requestedStateProps.indexOf(recordName) !== -1) {
-        mappedStateProps[byIds(type)] = providerState.recordsById;
+        // and 'typeById' i.e. 'todosById' is not present in mapped state
+        if (!(typeById in mapStateToProps)) {
+          // We bind 'typeById' i.e. 'todosById' to the mapped state
+          // The HOC will use this plus the passed `keyName` i.e. 'todoKey'
+          // to select the correct record
+          mappedStateProps[typeById] = providerState.recordsById;
+        }
       }
 
       return mappedStateProps;
     };
 
+    // Bind the mapping of the requested dispatch props to
+    // the actions available on the provider
     const mapDispatchToProps = dispatch => (
       bindActionCreators(
         zipObject(
@@ -73,23 +90,27 @@ export function provide(provider, merge) {
       )
     );
 
-    class WrappedComponent extends Component {
-      static propTypes = {};
-
+    // Wraping HOC
+    class WrappedComponent extends React.Component {
       render() {
+        // If 'recordName' i.e. 'todo' is in the requested state props
+        // we select it using the passed 'keyName' and the 'typeById' records
         if (requestedStateProps.indexOf(recordName) !== -1) {
           const props = {
             ...this.props,
-            [recordName]: this.props[byIds(type)][this.props[keyName]],
+            [recordName]: this.props[typeById][this.props[keyName]],
           };
 
-          if (requestedStateProps.indexOf(byIds(type)) === -1) {
-            return <Component {...omit(props, byIds(type))} />;
+          // If the user did not request the 'typeById' i.e. 'todosById'
+          // we remove it from the passed props
+          if (requestedStateProps.indexOf(typeById) === -1) {
+            return <Component {...omit(props, typeById)} />;
           }
 
           return <Component {...props} />;
         }
 
+        // Else just passed the props
         return <Component {...this.props} />;
       }
     }
