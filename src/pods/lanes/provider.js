baf54075d3675f1addd065a555f29f0022097e7c
Tyler Martinez <slightlytyler@gmail.com>
2016-02-25 14:54:08 -0600

    Use resource provider

--- src/pods/lanes/provider.js
+++ src/pods/lanes/provider.js
@@ -1,81 +1,16 @@
-// Constants
-const CREATE_LANE = 'CREATE_LANE';
+// Provider
+import { createResourceProvider } from 'react-redux-provide-pattern';
 
-export const constants = {
-  CREATE_LANE,
-};
+const lanes = createResourceProvider('lanes', 'lane', 'laneKey');
+const { SET_LANE } = lanes.constants;
 
 // Actions
 import generateId from 'shortid';
 
-export const actions = {
-  createLane(title) {
-    return {
-      type: CREATE_LANE,
-      id: generateId(),
-      title,
-    };
-  },
-};
-
-// Reducers
-import { combineReducers } from 'redux';
-
-export const reducers = combineReducers({
-  records(state = [], action) {
-    switch (action.type) {
-      case CREATE_LANE:
-        return [...state, action.id];
-
-      default:
-        return state;
-    }
-  },
-
-  recordsById(state = {}, action) {
-    switch (action.type) {
-      case CREATE_LANE: {
-        const { id, title } = action;
-        return Object.assign({}, state, {
-          [id]: {
-            id,
-            title,
-          },
-        });
-      }
-
-      default:
-        return state;
-    }
-  },
+lanes.actions.createLane = title => ({
+  type: SET_LANE,
+  id: generateId(),
+  payload: { title },
 });
-
-// Selectors
-import { createSelector } from 'reselect';
-
-const lanesSelector = state => state.lanes.records;
-const lanesByIdSelector = state => state.lanes.recordsById;
-
-const findLaneById = createSelector(
-  lanesByIdSelector,
-  (state, id) => id,
-  (lanesById, id) => lanesById[id],
-);
-
-export const selectors = {
-  lanesSelector,
-  lanesByIdSelector,
-  findLaneById,
-};
-
-// Provider
-import { createProvider } from 'react-redux-provide-pattern';
-
-const lanes = createProvider('lanes', 'lane', 'laneKey');
-
-lanes.constants = constants;
-lanes.actions = actions;
-lanes.reducers = reducers;
-lanes.selectors = selectors;
 
 export default lanes;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
-import { reducers as lanes } from 'pods/lanes/provider';
+import lanesProvider from 'pods/lanes/provider';
 
 export default combineReducers({
   router,
-  lanes,
+  lanes: lanesProvider.reducers,
 });
